8191771f5f029534ac479634914620b6d9fe8f90
Astrid Countee <astridwcountee@gmail.com>
2017-07-26 15:54:33 -0500

    updated with shout model. In the process of creating a shout on the dashboard

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,12 @@
 class HomesController < ApplicationController
+  before_filter :check_logged_in?
   def show
+  end
+
+  private
+  def check_logged_in?
+    if signed_in?
+      redirect_to dashboard_path
+    end
   end
 end
