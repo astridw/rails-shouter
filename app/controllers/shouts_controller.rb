4cb5e88e22d488c730446a0af2fdb5c236589bef
Astrid Countee <astridwcountee@gmail.com>
2017-08-15 20:35:06 -0500

    finished updating shouts, added alerts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -2,8 +2,16 @@ class ShoutsController < ApplicationController
 
   def create
     shout = current_user.shouts.build(shout_params)
-    shout.save
-    redirect_to dashboard_path
+   if shout.save
+     redirect_to dashboard_path
+   else
+     flash.alert = "Coult not Shout"
+      redirect_to dashboard_path
+    end
+  end
+
+  def show
+    @shout = Shout.find(params[:id])
   end
 
   private
